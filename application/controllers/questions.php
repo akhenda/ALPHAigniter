da062a0def610c42121af2ec3cc9d8b85b18ca4e
Joseph Akhenda <akhenda@gmail.com>
2016-06-25 00:52:56 +0300

    Added User and Questions controllers

---
+++ application/controllers/questions.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Questions extends CI_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+
+  function index()
+  {
+
+  }
+
+  public function add()
+  {
+      #code
+  }
+
+  public function detail()
+  {
+      #code
+  }
+
+  public function listing()
+  {
+      #code
+  }
+
+}
---
+++ application/controllers/users.php
@@ -0,0 +1,31 @@
+<?php
+class Users extends CI_Controller
+{
+
+    public function __construct ()
+    {
+        parent::__construct();
+    }
+
+    public function logout ()
+    {
+
+    }
+
+    /**
+     * Login a user and redirect him to questions
+     */
+    public function login ()
+    {
+        // TODO: Redirect user if already logged in
+        // TODO: Setup a login form
+        // TODO: Process the form
+            // TODO: Try to log in
+                // TODO: Redirect
+                // TODO: Error Message
+        // TODO: Set subview & load layout
+    }
+
+    public function register ()
+    {}
+}
