40e7b588035eb7d7ddf190ce27b54efb1d693d33
Joseph Akhenda <akhenda@gmail.com>
2016-06-24 17:59:29 +0300

    Added modular Ion Auth

---
+++ application/modules/auth/config/routes.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------------
+*/
+
+$route['auth/(:any)'] = "auth/$1";
+
+/* End of file routes.php */
+/* Location: ./application/config/routes.php */
+
+
+
