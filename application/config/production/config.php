7497d4db44243c7e3fb71a76ab51835e2062ac57
Joseph Akhenda <akhenda@gmail.com>
2016-06-24 22:56:19 +0300

    Updated the sess_save_path to sys_get_temp_dir()

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://alphaigniter.com/';
+$config['base_url'] = 'http://alphaigniter.com';
 
 /*
 |--------------------------------------------------------------------------
@@ -370,7 +370,7 @@ $config['encryption_key'] = 'htdf89pbk1qwhv6jp96g1bhd6yz9tj2jswqhazla5awvys0l50p
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
