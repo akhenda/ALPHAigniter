4e0a0c0e4a723359b27455ffc03c5fc115fc2751
Joseph Akhenda <akhenda@gmail.com>
2016-06-26 14:16:45 +0300

    Updated MY_Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -7,7 +7,7 @@ class MY_Controller extends MX_Controller {
     function __construct() {
         parent::__construct();
 
-        $this->load->library('auth/ion_auth');
+        // $this->load->library('auth/ion_auth');
 
         // Check authentication
         // $no_redirect = array('auth/login');
@@ -26,4 +26,38 @@ class MY_Controller extends MX_Controller {
         $data = array('subview' => $subview );
 		$this->load->view('layouts/layout', $data);
     }
+}
+
+
+ // --------------------------------------------------------------------------
+ /**
+  * Frontend Controller Deals with public pages that dont require authentication
+  */
+class Frontend_Controller extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+}
+
+
+// --------------------------------------------------------------------------
+/**
+ * Backend Controller provides a DRY implementation of auth required by access only pages
+ */
+class Backend_Controller extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+
+        $this->load->library('auth/ion_auth');
+
+        // Check authentication
+        // $no_redirect = array('auth/login');
+        // if ($this->ion_auth->logged_in() == false && ! in_array(uri_string(), $no_redirect)) {
+        //     redirect('auth/login');
+        // }
+    }
+
 }
