3499dd7fb7d436f0f48289e8e2ae6a3ceb0fcf5b
Joseph Akhenda <akhenda@gmail.com>
2016-06-26 11:02:26 +0300

    Updated all controller to extend MY_Controller

--- application/controllers/questions.php
+++ application/controllers/questions.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Questions extends CI_Controller{
+class Questions extends MY_Controller{
 
   public function __construct()
   {
--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,5 +1,5 @@
 <?php
-class Users extends CI_Controller
+class Users extends MY_Controller
 {
 
     public function __construct ()
