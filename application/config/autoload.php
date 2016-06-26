7a3ccd0b7d5d841ad3d989d03f6886ef2fed27c5
Joseph Akhenda <akhenda@gmail.com>
2016-06-26 10:19:56 +0300

    Autoloaded the general helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('date', 'form', 'html', 'url');
+$autoload['helper'] = array('date', 'form', 'html', 'url', 'general');
 
 /*
 | -------------------------------------------------------------------
