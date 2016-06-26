9b1feb5a24792505a4d02d47eda28cbe962a59ed
Joseph Akhenda <akhenda@gmail.com>
2016-06-26 10:18:02 +0300

    Created the general helper

---
+++ application/helpers/general_helper.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Dump helper. Functions to dump variables to the screen, in a nicley formatted manner.
+ * @author Joost van Veen
+ * @version 1.0
+ */
+if (!function_exists('dump')) {
+    function dump ($var, $label = 'Dump', $echo = TRUE)
+    {
+        // Store dump in variable 
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        
+        // Add formatting
+        $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
+        $output = '<pre style="background: #FFFEEF; color: #000; border: 1px dotted #000; padding: 10px; margin: 10px 0; text-align: left;">' . $label . ' => ' . $output . '</pre>';
+        
+        // Output
+        if ($echo == TRUE) {
+            echo $output;
+        }
+        else {
+            return $output;
+        }
+    }
+}
+
+
+if (!function_exists('dump_exit')) {
+    function dump_exit($var, $label = 'Dump', $echo = TRUE) {
+        dump ($var, $label, $echo);
+        exit;
+    }
+}
+
+/**
+ * Filter input based on a whitelist. This filter strips out all characters that
+ * are NOT: 
+ * - letters
+ * - numbers
+ * - Textile Markup special characters.
+ * 
+ * Textile markup special characters are:
+ * _-.*#;:|!"+%{}@
+ * 
+ * This filter will also pass cyrillic characters, and characters like ι and λ.
+ * 
+ * Typical usage:
+ * $string = '_ - . * # ; : | ! " + % { } @ abcdefgABCDEFG12345 ιό???????????????? ' . "\nAnd another line!";
+ * echo textile_sanitize($string);
+ * 
+ * @param string $string
+ * @return string The sanitized string
+ * @author Joost van Veen
+ */
+function textile_sanitize($string){
+    $whitelist = '/[^a-zA-Z0-9?-??-?ιό???????????????? \.\*\+\\n|#;:!"%@{} _-]/';
+    return preg_replace($whitelist, '', $string);
+}
+
+function escape($string){
+    return textile_sanitize($string);
+}
+
