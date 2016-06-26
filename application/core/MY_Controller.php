fc53443fbc5dea18fc97ac74dc7e40545aad501b
Joseph Akhenda <akhenda@gmail.com>
2016-06-26 11:08:17 +0300

    Loaded Ion Auth library and added an authentication check

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,5 +2,13 @@
 class MY_Controller extends CI_Controller {
     function __construct () {
         parent::__construct();
+
+        $this->load->library('auth/ion_auth');
+
+        // Check authentication
+        $no_redirect = array('users/login');
+        if ($this->ion_auth->logged_in() == false && ! in_array(uri_string(), $no_redirect)) {
+            redirect('users/login');
+        }
     }
 }
