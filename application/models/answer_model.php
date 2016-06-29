d0a3a450a2d868196525789682d6f569ffc99537
Joseph Akhenda <akhenda@gmail.com>
2016-06-29 13:26:43 +0300

    Added an insert function that uses MY_Model insert()

--- application/models/answer_model.php
+++ application/models/answer_model.php
@@ -7,27 +7,29 @@ class Answer_model extends MY_Model {
         array('field' => 'text', 'label' => 'Answer', 'rules' => 'trim|sanitize'),
          );
 
-    public function __construct () {
-        parent::__construct();
-
+    public function __construct() {
+        $this->table = 'answers';
+        $this->primary_key = 'id';
         $this->has_one['user'] = array('foreign_model'=>'user_model','foreign_table'=>'users','foreign_key'=>'id','local_key'=>'user_id');
         $this->has_one['question'] = array('foreign_model'=>'question_model','foreign_table'=>'questions','foreign_key'=>'id','local_key'=>'questions_id');
 
         $this->_database = $this->db;
+
+        parent::__construct();
     }
 
     /**
      * Insert an answer from POST values
      * @see MY_Model::insert()
      */
-    // public function insert ()
-    // {
-    //     $data = array(
-    //         'user_id' => $this->input->post('user_id'),
-    //         'questions_id' => $this->input->post('questions_id'),
-    //         'text' => $this->input->post('text'),
-    //     );
-    //     //$this->load->model('answer_model');
-    //     //$this->answer_model->insert($data);
-    // }
+    public function insert($data = NULL) {
+        $data = array(
+            'user_id' => $this->input->post('user_id'),
+            'questions_id' => $this->input->post('questions_id'),
+            'text' => $this->input->post('text')
+        );
+        // $this->load->model('answer_model');
+        // $this->answer_model->insert($data);
+        parent::insert($data);
+    }
 }
