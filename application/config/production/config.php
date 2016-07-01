510c71280f84e97f2f1866af29adf2f79277eb1e
Joseph Akhenda <akhenda@gmail.com>
2016-07-02 00:00:46 +0300

    Changed the session driver to database and the save path to the ci_sessions table

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -367,10 +367,10 @@ $config['encryption_key'] = 'htdf89pbk1qwhv6jp96g1bhd6yz9tj2jswqhazla5awvys0l50p
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'cisession';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
